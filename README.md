ef0d1be37e5fc810c8bc970148b0934a266ed653
Anthony Dillon <me@anthonydillon.com>
2020-09-04 00:00:07 +0100

    Inital build of the site

---
+++ README.md
@@ -0,0 +1,18 @@
+# charmed-kubeflow.io
+
+[![Code coverage](https://codecov.io/gh/canonical-web-and-design/charmed-kubeflow.io/branch/master/graph/badge.svg)](https://codecov.io/gh/canonical-web-and-design/charmed-kubeflow.io)
+
+This is the repo for the [charmed-kubeflow.io website](https://charmed-kubeflow.io).
+
+## Usage
+
+After [installing Docker](https://docs.docker.com/install/):
+
+``` bash
+./run
+```
+
+And browse to http://127.0.0.1:8037.
+
+# Deploy
+You can find the deployment config in the deploy folder.
---
+++ webapp/app.py
@@ -0,0 +1,51 @@
+import talisker.requests
+
+# Packages
+from canonicalwebteam.flask_base.app import FlaskBase
+from canonicalwebteam import image_template
+from canonicalwebteam.discourse_docs import (
+    DiscourseDocs,
+    DiscourseAPI,
+    DocParser,
+)
+from flask import render_template
+
+# Rename your project below
+app = FlaskBase(
+    __name__,
+    "charmed-kubeflow.io",
+    template_folder="../templates",
+    static_folder="../static",
+    template_404="404.html",
+    template_500="500.html",
+)
+session = talisker.requests.get_session()
+
+
+doc_parser = DocParser(
+    api=DiscourseAPI(base_url="https://discourse.dqlite.io/", session=session),
+    index_topic_id=21,
+    category_id=5,
+    url_prefix="/docs",
+)
+
+if app.debug:
+    doc_parser.api.session.adapters["https://"].timeout = 99
+
+discourse_docs = DiscourseDocs(
+    parser=doc_parser,
+    document_template="docs/document.html",
+    url_prefix="/docs",
+)
+discourse_docs.init_app(app)
+
+
+@app.context_processor
+def utility_processor():
+    return {"image": image_template}
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
