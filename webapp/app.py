105d0f65f35c1f5a90bd63826de1bd808d6da843
Pete <peterfrench94@gmail.com>
2024-09-19 13:57:18 +0200

    chore: Upgrade canonicalwebteam.search to 2.1.1 & upgrade canonicalwebteam.flask-base to 2.0.0

--- webapp/app.py
+++ webapp/app.py
@@ -38,9 +38,11 @@ app.add_url_rule(
     "/docs/search",
     "docs-search",
     build_search_view(
+        app=app,
         session=session,
         site="charmed-kubeflow.io/docs",
         template_path="docs/search.html",
+        request_limit="1/day",
     ),
 )
 
